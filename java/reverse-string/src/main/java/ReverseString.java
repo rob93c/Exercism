ac6282aa43d2fa62c96a409782c93885d3aadce2
Roberto <rob.uniud@gmail.com>
2019-03-14 18:36:13 +0100

    Added a method to reverse strings in Java

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,23 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+    	if(inputString == "") {
+    		return inputString;
+    	} else if(inputString == null) {
+            throw new IllegalArgumentException("The string can't be null.");
+    	} else {
+    		char[] input  = inputString.toCharArray();
+			int start = 0;
+			int end = input.length-1;
+			char temp;
+    		while(end>start) {
+    			temp = input[start];
+    			input[start] = input[end];
+    			input[end] = temp;
+    			end--;
+    			start++;
+    		}
+    		return new String(input);
+    	}
+    }
+}
