6028de88ed5fa2f02b8387d96aeddce7c80ad353
Roberto <rob.uniud@gmail.com>
2019-03-21 16:39:25 +0100

    Difference of squares calculator added

---
+++ java/difference-of-squares/src/main/java/DifferenceOfSquaresCalculator.java
@@ -0,0 +1,25 @@
+public class DifferenceOfSquaresCalculator {
+
+	private int squareOfSum;
+
+    public int computeSquareOfSumTo(int input) {
+        int sum = 0;
+        for(int i = 1; i <= input; i++) {
+        	sum += i;
+        	squareOfSum = sum * sum;
+        }
+        return squareOfSum;
+    }
+
+    public int computeSumOfSquaresTo(int input) {
+        int sum = 0;
+        for(int i = 1; i <= input; i++) {
+        	sum += (i * i);
+        }
+        return sum;
+    }
+
+    public int computeDifferenceOfSquares(int input) {
+        return computeSquareOfSumTo(input) - computeSumOfSquaresTo(input);
+    }
+}
