fde36a2e1e071a7afb1e61757535cbffb87d10f7
Roberto <rob.uniud@gmail.com>
2019-03-25 17:33:22 +0100

    Removed useless code

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -3,7 +3,7 @@ public class Triangle {
     private double side1, side2, side3;
 
     public Triangle(double side1, double side2, double side3) throws TriangleException {
-        if(side1 == 0 || side2 == 0 || side3 == 0 || side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
+        if(side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
             throw new TriangleException();
         }
         this.side1 = side1;
@@ -11,13 +11,13 @@ public class Triangle {
         this.side3 = side3;
     }
 
-    boolean isEquilateral() {
+    public boolean isEquilateral() {
         if(side1 == side2 && side1 == side3) {
             return true;
         } else return false;
     }
 
-    boolean isIsosceles() {
+    public boolean isIsosceles() {
         if((side1 == side2 && side1 == side3) ||
            (side1 == side2 && side1 != side3) || 
            (side1 == side3 && side1 != side2) || 
@@ -26,7 +26,7 @@ public class Triangle {
         } else return false;
     }
 
-    boolean isScalene() {
+    public boolean isScalene() {
         if(side1 != side2 && side1 != side3) {
             return true;
         } else return false;
