c6d124789bfcc114cd339dc0934fddc51085f311
Roberto <rob.uniud@gmail.com>
2019-03-25 17:26:02 +0100

    Triangle solution added

---
+++ java/triangle/src/main/java/Triangle.java
@@ -0,0 +1,35 @@
+public class Triangle {
+
+    private double side1, side2, side3;
+
+    public Triangle(double side1, double side2, double side3) throws TriangleException {
+        if(side1 == 0 || side2 == 0 || side3 == 0 || side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
+            throw new TriangleException();
+        }
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    boolean isEquilateral() {
+        if(side1 == side2 && side1 == side3) {
+            return true;
+        } else return false;
+    }
+
+    boolean isIsosceles() {
+        if((side1 == side2 && side1 == side3) ||
+           (side1 == side2 && side1 != side3) || 
+           (side1 == side3 && side1 != side2) || 
+           (side2 == side3 && side2 != side1)) {
+            return true;
+        } else return false;
+    }
+
+    boolean isScalene() {
+        if(side1 != side2 && side1 != side3) {
+            return true;
+        } else return false;
+    }
+
+}
