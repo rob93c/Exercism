25e7ba292fce220f502a80794db31be0d35927d0
Roberto <rob.uniud@gmail.com>
2019-03-26 14:45:11 +0100

    Saddle-points added

---
+++ java/saddle-points/src/main/java/Matrix.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Matrix {
+
+    private List<List<Integer>> matrix;
+
+    Matrix(List<List<Integer>> values) {
+        matrix = values;
+    }
+
+    Set<MatrixCoordinate> getSaddlePoints() {
+        Set<MatrixCoordinate> result = new HashSet<>();
+        for (int r = 0; r < matrix.size(); r++) {
+            int hi = Collections.max(matrix.get(r));
+            for (int c = 0; c < matrix.get(r).size(); c++)
+                if (matrix.get(r).get(c) == hi) {
+                    int lo = matrix.get(r).get(c);
+                    for (List<Integer> aMatrix : matrix)
+                        if (aMatrix.get(c) < hi)
+                            lo = aMatrix.get(c);
+                    if (lo == hi)
+                        result.add(new MatrixCoordinate(r, c));
+                }
+        }
+        return result;
+    }
+}
