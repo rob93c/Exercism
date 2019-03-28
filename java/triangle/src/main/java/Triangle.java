a0cad04f1394f4332c1514d4a968b3568bcdd8f6
Roberto <rob.uniud@gmail.com>
2019-03-28 12:52:38 +0100

    Fixes

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -3,7 +3,9 @@ public class Triangle {
     private double side1, side2, side3;
 
     public Triangle(double side1, double side2, double side3) throws TriangleException {
-        if(side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2) {
+        if(side1 + side2 <= side3 ||
+           side2 + side3 <= side1 || 
+           side3 + side1 <= side2) {
             throw new TriangleException();
         }
         this.side1 = side1;
