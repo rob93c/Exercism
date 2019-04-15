3401a4533884b534f614c8dea309e421f0cbf08f
Roberto <rob.uniud@gmail.com>
2019-04-15 11:06:40 +0200

    Added High Score exercise

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,14 @@
+class HighScores(object):
+    def __init__(self, scores):
+        self.scores = scores
+
+    def latest(self):
+    	return self.scores[-1]
+
+    def personal_best(self):
+    	self.scores.sort(reverse = True)
+    	return self.scores[0]	
+
+    def personal_top_three(self):
+    	self.scores.sort(reverse = True)
+    	return self.scores[:3]    	
