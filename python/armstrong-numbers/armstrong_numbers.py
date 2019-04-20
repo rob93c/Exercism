c521d9dd90297af89a98c44ff248481e5cf68f09
Roberto <rob.uniud@gmail.com>
2019-04-20 10:37:46 +0200

    Cleaned code

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,9 +1,8 @@
 def is_armstrong(number):
     strnum = str(number)
     count = 0
-
-    for i in range(len(strnum)):
-        count += int(strnum[i]) ** len(strnum)
+    for i in strnum:
+        count += int(i) ** len(strnum)
     if count == number:
         return True
     else:
