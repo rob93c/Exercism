b0ef5c272cce1f191a0d6859a2d5dc6539196db2
Roberto <rob.uniud@gmail.com>
2019-04-23 14:10:47 +0200

    Raindrops added + anagram syntax improvements

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,6 +1,4 @@
 def find_anagrams(word, candidates):
-    result = []
-    for i in candidates:
-    	if i.lower() != word.lower() and sorted(i.lower()) == sorted(word.lower()):
-    		result.append(i)
+    result = [i for i in candidates if i.lower() != word.lower(
+    ) and sorted(i.lower()) == sorted(word.lower())]
     return result
